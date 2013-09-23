a1dc066121309b0f29a16ee2616695ae52487a63
Marko Dimjašević <mdimjasevic@users.noreply.github.com>
2013-09-23 10:50:53 -0600

    Documentation: mentioned that a user should clean up after JPF-Doop

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A script that cleans up after JPF-Doop
+
+
+rm -rf tests-round-*
+rm -rf build/
+rm -f concrete-values*
+rm -f classlist.txt
+rm -rf jacoco-site/
+rm -f jdart-regular-executions.txt
+rm -rf randooped*
